04a591ca224a577375bdd366959d6d67e654bf90
Sapia <123062954+sapiaf@users.noreply.github.com>
2023-09-05 21:39:27 +0200

    Exercise completed

--- src/com/learn/java_programming/part4/exercise25/GuestListFromAFile.java
+++ src/com/learn/java_programming/part4/exercise25/GuestListFromAFile.java
@@ -1,37 +1,36 @@
 package com.learn.java_programming.part4.exercise25;
 
-import java.io.IOException;
-import java.nio.file.NoSuchFileException;
+
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class GuestListFromAFile {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        List<String> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
         System.out.println("Name of the file: ");
-        String fileName = scanner.nextLine();
+        String fileName = sc.nextLine();
 
-        try (Scanner scanner1 = new Scanner(Paths.get("src/com/learn/java_programming/part4/exercise25/" + fileName))) {
-            while (scanner1.hasNextLine()) {
-                String line = scanner1.nextLine();
-                if (line.isEmpty()) continue;
-                list.add(line);
+        try (Scanner scanner = new Scanner(Paths.get("src/com/learn/java_programming/part4/exercise25/" + fileName))) {
+            while (scanner.hasNext()) {
+                String row = scanner.nextLine();
+                names.add(row);
             }
-        } catch (NoSuchFileException noSuchFileException) {
-            System.out.println("Error: The file " + fileName + " was not found.");
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
 
-        System.out.println("Enter names, an empty line quits.");
         while (true) {
-            String name = scanner.nextLine();
+            System.out.println("Enter names, an empty line quits.");
+            String name = sc.nextLine().toLowerCase();
             if (name.isEmpty()) break;
-            if (list.contains(name)) System.out.println("The name is on the list.");
-            else System.out.println("The name is not on the list.");
+            if (names.contains(name)) {
+                System.out.println("The name is on the list.");
+            } else {
+                System.out.println("The name is not on the list.");
+            }
         }
+
     }
 }
--- src/com/learn/java_programming/part4/exercise26/IsItInTheFile.java
+++ src/com/learn/java_programming/part4/exercise26/IsItInTheFile.java
@@ -8,30 +8,29 @@ import java.util.Scanner;
 
 public class IsItInTheFile {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        List<String> list = new ArrayList<>();
-
+        Scanner sc = new Scanner(System.in);
         System.out.println("Name of the file: ");
-        String fileName = scanner.nextLine();
-
+        String fileName = sc.nextLine();
         System.out.println("Search for: ");
-        String str = scanner.nextLine();
+        String name = sc.nextLine();
+        boolean found = false;
 
-        try (Scanner scanner2 = new Scanner(Paths.get("src/com/learn/java_programming/part4/exercise25/" + fileName))) {
-            while (scanner2.hasNextLine()) {
-                String row = scanner2.nextLine();
-                if (row.isEmpty()) continue;
-                list.add(row);
+        try (Scanner scanner = new Scanner(Paths.get("src/com/learn/java_programming/part4/exercise26/" + fileName))) {
+            while (scanner.hasNext()) {
+                if (scanner.nextLine().equalsIgnoreCase(name)) {
+                    found = true;
+                    break;
+                }
             }
-        } catch (NoSuchFileException noSuchFileException) {
-            System.out.println("Reading the file " + fileName + " failed.");
-            return;
         } catch (Exception e) {
-            System.out.println("An error occurred: " + e.getMessage());
-            return;
+            System.out.println("Reading the file " + fileName + " failed.");
         }
 
-        if (list.contains(str)) System.out.println("Found!");
-        else System.out.println("Not Found");
+        if (found) {
+            System.out.println("Found!");
+        } else {
+            System.out.println("Not found!");
+        }
+
     }
 }
