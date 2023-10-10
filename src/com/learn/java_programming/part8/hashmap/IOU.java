aa897858421a9bf0f2f3c52102e389d37a29454c
Francesco Sapia <123062954+sapiaf@users.noreply.github.com>
2023-10-10 22:24:49 +0200

    Java Programming Part. 8 HashMap chapter completed

---
+++ src/com/learn/java_programming/part8/hashmap/IOU.java
@@ -0,0 +1,26 @@
+package com.learn.java_programming.part8.hashmap;
+
+import java.util.HashMap;
+
+public class IOU {
+    private HashMap<String, Double> list;
+
+    public IOU() {
+        this.list = new HashMap<>();
+    }
+
+    public void setSum(String toWhom, double amount) {
+        list.put(toWhom, amount);
+    }
+    public double howMuchDoIOweTo(String toWhom) {
+        return list.get(toWhom);
+    }
+
+    public static void main(String[] args) {
+        IOU mattsIOU = new IOU();
+        mattsIOU.setSum("Arthur", 51.5);
+        mattsIOU.setSum("Arthur", 10.5);
+
+        System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));
+    }
+}
